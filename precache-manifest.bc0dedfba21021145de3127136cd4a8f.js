d5f43ff8c742f578c2176ebc66a8b324b56c9ba5
Patryk Szlachta <patryk.szla@interia.pl>
2019-11-05 16:41:14 +0100

    Updates

---
+++ precache-manifest.bc0dedfba21021145de3127136cd4a8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ead427a41517a5164e376484b907bd4d",
+    "url": "/users-list/index.html"
+  },
+  {
+    "revision": "2baebf45a6941f009fef",
+    "url": "/users-list/static/css/main.1b980d53.chunk.css"
+  },
+  {
+    "revision": "f71c3e6418c9c91ee4ba",
+    "url": "/users-list/static/js/2.a6157bb6.chunk.js"
+  },
+  {
+    "revision": "2baebf45a6941f009fef",
+    "url": "/users-list/static/js/main.64cd333a.chunk.js"
+  },
+  {
+    "revision": "d3746a31656d1398add5",
+    "url": "/users-list/static/js/runtime-main.5bc0ea3b.js"
+  }
+]);
